575bfd9fa2d6c11f9abdf33f166053bbb2805227
m_kimura <chitosws.apple@gmail.com>
2023-05-31 23:32:05 +0900

    Initial Commit

---
+++ Scanner/ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScannerApp.swift
+//  Scanner
+//
+//  Created by Motoki Kimura on 2023/05/31.
+//
+
+import SwiftUI
+
+@main
+struct ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
